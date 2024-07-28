62a8210c7bb20ab1667a49d6dbde8a4df3fcdc0c
omerkisa1 <omereis.5555@gmail.com>
2024-07-28 14:23:59 +0300

    knn5 compare with knn4

---
+++ KNN5.py
@@ -0,0 +1,37 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+cancer = load_breast_cancer()
+
+df = pd.DataFrame(data = cancer.data, columns = cancer.feature_names) # dataset
+df["target"] = cancer.target
+
+#print(df) 
+
+X = cancer.data # features
+y = cancer.target # target
+
+#train test split
+#We simply generated test data and calculated accuracy with it. compare with knn2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+#Scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train,y_train)
+    
+
+y_pred = knn.predict(X_test) # pred data
+
+# We need reel data [y] , pred data[y_pred] for accuracy params
+accuracy = accuracy_score(y_test, y_pred)
+
+#confusion calculation
+conf_matrix = confusion_matrix(y_test, y_pred)
